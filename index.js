8426290399389594fc2b6e66853a379c52d69b74
Albertojs89 <jimenezsanchezalberto@fpllefia.com>
2024-11-02 11:53:33 +0100

    portfolio

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hola")
+
+//array de cards con titulo y descripción
+
+const myArray = [
+  {
+    titulo: "Concept 1",
+    imagen: "urlImagen",
+    description: "Descripción del concept 1",
+  },
+  {
+    titulo: "Concept 2",
+    imagen: "urlImagen",
+    description: "Descripción del concept 2",
+  },
+  {
+    titulo: "Concept 3",
+    imagen: "urlImagen",
+    description: "Descripción del concept 3",
+  }
+];
+
+//----------------------------------------------------------------
+
+//efecto deslizar y que aparece el contenido:
+
+
+
+
